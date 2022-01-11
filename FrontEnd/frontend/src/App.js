7d885b74ad0651355a13c5353d22aba826f9b933
LazyCORE <93406721+LazyCORE@users.noreply.github.com>
2022-01-12 00:52:47 +0200

    Add react

---
+++ FrontEnd/frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>Test</h1>
+
+      </header>
+      <div className="layoout">
+        <div>Test Test</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
